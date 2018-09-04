afab349614d4a3196fe6740a78a853974c7f75ea
Peter Blum <pblum@pivotal.io>
2018-09-04 07:55:09 -0500

    add integration tests

--- ci-cd/tasks/integration-test/task.sh
+++ ci-cd/tasks/integration-test/task.sh
@@ -5,7 +5,13 @@ cf api $CF_API --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
 cf apps
 cf services
-cf curl "/v2/apps?q=organization_guid:$(cf org "$CF_ORG" --guid)"
 
+curl -k https://"$CF_SPACE"-web-"$CF_ORG".cfapps.haas-115.pez.pivotal.io
+curl -k https://"$CF_SPACE"-accounts-"$CF_ORG".cfapps.haas-115.pez.pivotal.io
+curl -k https://"$CF_SPACE"-quote-"$CF_ORG".cfapps.haas-115.pez.pivotal.io
+curl -k https://"$CF_SPACE"-user-"$CF_ORG".cfapps.haas-115.pez.pivotal.io
+curl -k https://"$CF_SPACE"-portfolio-"$CF_ORG".cfapps.haas-115.pez.pivotal.io
+
+echo "All production services are up and green!! :)"
 set -xe
 
