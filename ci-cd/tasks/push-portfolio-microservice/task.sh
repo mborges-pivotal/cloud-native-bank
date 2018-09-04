3e0c68f227b3e94a7c9eb00cb41b7cce4df530c8
Peter Blum <Oskoss@users.noreply.github.com>
2018-09-04 07:30:46 -0500

    Update task.sh

--- ci-cd/tasks/push-portfolio-microservice/task.sh
+++ ci-cd/tasks/push-portfolio-microservice/task.sh
@@ -2,7 +2,7 @@
 set -xe
 
 cd pivotal-bank-demo/portfolio-service
-./gradlew build -DskipTests
+./gradlew build -x test
 cf api $CF_API --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
 cf push
