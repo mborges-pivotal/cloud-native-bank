d7b0aad51bb13c3cd72c89855308fc74c961c060
Vince Russo <vrussoiii@pivotal.io>
2018-09-04 03:41:25 -0400

    Skipped all tests

--- ci-cd/tasks/push-web-microservice/task.sh
+++ ci-cd/tasks/push-web-microservice/task.sh
@@ -2,7 +2,7 @@
 set -xe
 
 cd pivotal-bank-demo/web-ui
-./gradlew build
+./gradlew build -DskipTests
 cf api $CF_API --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
 cf push
