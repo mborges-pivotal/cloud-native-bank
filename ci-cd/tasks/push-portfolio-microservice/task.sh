3b47034f41c7221bcc4e5c6255f532c7c9b8f855
Vince Russo <vrussoiii@pivotal.io>
2018-09-04 03:17:19 -0400

    Added the rest of the services

---
+++ ci-cd/tasks/push-portfolio-microservice/task.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xe
+
+cd pivotal-bank-demo/portfolio-service
+./gradlew build
+cf api $CF_API --skip-ssl-validation
+cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
+cf push
+
+set -xe
+
