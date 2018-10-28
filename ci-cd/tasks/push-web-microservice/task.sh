19575e9cbb430160b373852ef1f7cde4326f4099
mborges-pivotal <mborges@pivotal.io>
2018-10-28 01:57:21 -0500

    add parameters for domain

--- ci-cd/tasks/push-web-microservice/task.sh
+++ ci-cd/tasks/push-web-microservice/task.sh
@@ -6,8 +6,8 @@ cd pivotal-bank-demo/web-ui
 cf api $CF_API --skip-ssl-validation
 cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
 cf push  prod-web --no-route
-cf create-route "$CF_SPACE" cfapps.haas-115.pez.pivotal.io --hostname prod-web-"$CF_ORG"
-cf map-route prod-web cfapps.haas-115.pez.pivotal.io --hostname prod-web-"$CF_ORG"
+cf create-route "$CF_SPACE" "$CF_DOMAIN" --hostname prod-web-"$CF_ORG"
+cf map-route prod-web "$CF_DOMAIN" --hostname prod-web-"$CF_ORG"
 
 
 set -xe
