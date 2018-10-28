d9264b8640188a62baeb69b1c291f3b69d4beb21
mborges-pivotal <mborges@pivotal.io>
2018-10-28 03:00:39 -0500

    fix var interpolation

--- ci-cd/tasks/integration-test/task.sh
+++ ci-cd/tasks/integration-test/task.sh
@@ -6,11 +6,11 @@ cf login -u $CF_USER -p $CF_PWD -o "$CF_ORG" -s "$CF_SPACE"
 cf apps
 cf services
 
-curl -k https://"$CF_SPACE"-web-"{$CF_ORG},{$DOMAIN}"
-curl -k https://"$CF_SPACE"-accounts-"{$CF_ORG},{$DOMAIN}"
-curl -k https://"$CF_SPACE"-quote-"{$CF_ORG},{$DOMAIN}"
-curl -k https://"$CF_SPACE"-user-"{$CF_ORG},{$DOMAIN}"
-curl -k https://"$CF_SPACE"-portfolio-"{$CF_ORG},{$DOMAIN}"
+curl -k https://"$CF_SPACE"-web-"${CF_ORG}.${DOMAIN}"
+curl -k https://"$CF_SPACE"-accounts-"${CF_ORG}.${DOMAIN}"
+curl -k https://"$CF_SPACE"-quote-"${CF_ORG}.${DOMAIN}"
+curl -k https://"$CF_SPACE"-user-"${CF_ORG}.${DOMAIN}"
+curl -k https://"$CF_SPACE"-portfolio-"${CF_ORG}.${DOMAIN}"
 
 echo "All production services are up and green!! :)"
 set -xe
